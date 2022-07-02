51b1afcc81031042a132473296489360be37b3c3
bandoss81 <bandoss81@gmail.com>
2022-07-02 13:37:52 +0300

    Example 012 Команда (for)

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -53,6 +53,7 @@ void Method21(string msg, int count)
 
 
 // Вид 4
+/*
 string Method4(int count, string text)
 {
     int i = 0;
@@ -69,19 +70,31 @@ string Method4(int count, string text)
 
 string res = Method4(10, "yes ");
 Console.WriteLine(res);
+*/
+
+string Method4(int count, string text)
+{
+    string result = String.Empty;
+    for(int i = 0;i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+
+}
+
+string res = Method4(10, "yes ");
+//Console.WriteLine(res);
 
 
-
-
-
-
-
-
-
-
-
-
-
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
 
 
 
